f55e01bb5f7de47352ad1aeb3d515b71ebae0a3e
yadavharsha <yadavharsha227@gmail.com>
2023-07-24 14:16:54 +0530

    both updated

--- Dbutils.java
+++ Dbutils.java
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
+
 it's a nice thing the code is not moved here 
-
-
-=======
-    adding or updating
->>>>>>> development
+harshayadav
+    adding or updating 
 adding the DB file it is very important
 hello this file contains DB connection logic
 
